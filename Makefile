59aa1a611143656bb74bf73bda2e8d0ecfcefe07
Bagmanov.P <bagman-2008@yandex.ru>
2015-11-11 11:12:16 +0300

    "fix make file and certigicate"

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ vmime=`pkg-config --cflags --libs vmime`
 
 all: myprog
 
-myprog: main.o MailBox.o Message.o NotifyMessage.o MailBoxSetting.o
-	g++ -std=c++11 main.o MailBox.o Message.o NotifyMessage.o MailBoxSetting.o $(vmime) $(notify)-o myprog
+myprog: main.o MailBox.o Message.o NotifyMessage.o MailBoxSetting.o certificateVerifier.o
+	g++ -std=c++11 main.o MailBox.o Message.o NotifyMessage.o MailBoxSetting.o certificateVerifier.o $(vmime) $(notify)-o myprog
 main.o: main.cpp MailBox.hpp Message.hpp NotifyMessage.hpp MailBoxSetting.hpp
 	g++ -std=c++11 -c main.cpp
 MailBox.o: MailBox.cpp Message.hpp MailBoxSetting.hpp MailBox.hpp timeoutHandler.hpp
@@ -17,5 +17,7 @@ NotifyMessage.o: NotifyMessage.cpp NotifyMessage.hpp Message.hpp
 	g++ -std=c++11 -c $(glib) $(gdk) NotifyMessage.cpp
 MailBoxSetting.o: MailBoxSetting.cpp MailBoxSetting.hpp
 	g++ -std=c++11 -c MailBoxSetting.cpp
+certificateVerifier.o: certificateVerifier.cpp certificateVerifier.hpp
+	g++ -std=c++11 -c certificateVerifier.cpp
 clean:
 	rm -rf *.o myprog
--- certificateVerifier.cpp
+++ certificateVerifier.cpp
@@ -1,42 +1,46 @@
-#include "certificateVerifier.hpp"
 #include <vmime/vmime.hpp>
 #include <iostream>
+
+#include "certificateVerifier.hpp"
+
+std::vector <vmime::ref <vmime::security::cert::X509Certificate> >
+	interactiveCertificateVerifier::m_trustedCerts;
 void interactiveCertificateVerifier::verify(vmime::ref <vmime::security::cert::certificateChain> chain)
 {
 	try
-	{
+		{
 			setX509TrustedCerts(m_trustedCerts);
 
 			defaultCertificateVerifier::verify(chain);
-	}
-	catch (vmime::exceptions::certificate_verification_exception&)
-	{
-		// Obtain subject's certificate
-		vmime::ref <vmime::security::cert::certificate> cert = chain->getAt(0);
-
-		std::cout << std::endl;
-		std::cout << "Server sent a '" << cert->getType() << "'" << " certificate." << std::endl;
-		std::cout << "Do you want to accept this certificate? (Y/n) ";
-		std::cout.flush();
-
-		std::string answer;
-		std::getline(std::cin, answer);
-
-		if (answer.length() != 0 && (answer[0] == 'Y' || answer[0] == 'y'))
+		}
+		catch (vmime::exceptions::certificate_verification_exception&)
 		{
-				// Accept it, and remember user's choice for later
-			if (cert->getType() == "X.509")
+			// Obtain subject's certificate
+			vmime::ref <vmime::security::cert::certificate> cert = chain->getAt(0);
+
+			std::cout << std::endl;
+			std::cout << "Server sent a '" << cert->getType() << "'" << " certificate." << std::endl;
+			std::cout << "Do you want to accept this certificate? (Y/n) ";
+			std::cout.flush();
+
+			std::string answer;
+			std::getline(std::cin, answer);
+
+			if (answer.length() != 0 &&
+			    (answer[0] == 'Y' || answer[0] == 'y'))
 			{
-				m_trustedCerts.push_back(cert.dynamicCast
-					<vmime::security::cert::X509Certificate>());
+				// Accept it, and remember user's choice for later
+				if (cert->getType() == "X.509")
+				{
+					m_trustedCerts.push_back(cert.dynamicCast
+						<vmime::security::cert::X509Certificate>());
+				}
+
+				return;
 			}
 
-			return;
+			throw vmime::exceptions::certificate_verification_exception
+				("User did not accept the certificate.");
 		}
-
-		throw vmime::exceptions::certificate_verification_exception
-			("User did not accept the certificate.");
-	}
 }
-
 
