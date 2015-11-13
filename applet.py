f4417837e9c50c2323a64962b45af43d9baef82a
Bagmanov.P <bagman-2008@yandex.ru>
2015-11-14 00:07:56 +0300

    "eternal cycle"

--- applet.py
+++ applet.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
+from gi.repository import GLib
 from messagesmodule import *
 
-def app_let_indicator():
-    ind = appindicator.Indicator.new(
-        "example-simple-client",
-        "indicator-messages",
-        appindicator.IndicatorCategory.APPLICATION_STATUS)
-    ind.set_status(appindicator.IndicatorStatus.ACTIVE)
-    ind.set_attention_icon("indicator-messages-new")
 
-    menu = Gtk.Menu()
+class Applet:
+    def __init__(self, timeout, limit=5):
+        self.timeout = timeout
+        self.limit = limit
+        self.count = 0
+    def __call__(self):
+        while True:
+            ind = appindicator.Indicator.new("example-simple-client", "indicator-messages", appindicator.IndicatorCategory.APPLICATION_STATUS)
+            ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+            ind.set_attention_icon("indicator-messages-new")
 
-    for el in getMessages():
-        buf = el
-        menu_items = Gtk.MenuItem(buf)
-        menu.append(menu_items)
-        menu_items.show()
-    ind.set_menu(menu)
-    Gtk.main()
+            menu = Gtk.Menu()
+            
+            for email in getMessages():
+                menu_items = Gtk.MenuItem(email)
+                menu.append(menu_items)
+                menu_items.show()
+           
+            ind.set_menu(menu)
+            GLib.timeout_add_seconds(self.timeout, self.end)
+            Gtk.main()
+            if self.count == self.limit:
+                break
 
-app_let_indicator();
+    def end(self):
+        self.count += 1
+        Gtk.main_quit()
+
+applet = Applet(15)
+applet()
--- certificateVerifier.cpp
+++ certificateVerifier.cpp
@@ -20,24 +20,15 @@ void interactiveCertificateVerifier::verify(vmime::ref <vmime::security::cert::c
 
 			std::cout << std::endl;
 			std::cout << "Server sent a '" << cert->getType() << "'" << " certificate." << std::endl;
-			std::cout << "Do you want to accept this certificate? (Y/n) ";
-			std::cout.flush();
 
-			std::string answer;
-			std::getline(std::cin, answer);
-
-			if (answer.length() != 0 &&
-			    (answer[0] == 'Y' || answer[0] == 'y'))
+			// Accept it, and remember user's choice for later
+			if (cert->getType() == "X.509")
 			{
-				// Accept it, and remember user's choice for later
-				if (cert->getType() == "X.509")
-				{
-					m_trustedCerts.push_back(cert.dynamicCast
-						<vmime::security::cert::X509Certificate>());
-				}
-
-				return;
+				m_trustedCerts.push_back(cert.dynamicCast
+				<vmime::security::cert::X509Certificate>());
 			}
+
+			return;
 
 			throw vmime::exceptions::certificate_verification_exception
 				("User did not accept the certificate.");
